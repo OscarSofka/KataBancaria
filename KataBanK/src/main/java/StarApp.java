9e1d33132dbb03e39d25bdc3fa4ed74800c4144b
OscarSofka <107581543+OscarSofka@users.noreply.github.com>
2022-06-21 14:24:23 -0500

    KAtaBank

---
+++ KataBanK/src/main/java/StarApp.java
@@ -0,0 +1,9 @@
+import co.com.sofka.Account;
+
+public class StarApp {
+    public static void main(String[] args){
+        Account account = new Account();
+
+
+    }
+}
---
+++ KataBanK/src/main/java/co/com/sofka/Account.java
@@ -0,0 +1,11 @@
+package co.com.sofka;
+
+public class Account {
+    private int amountOf;
+    private String date;
+
+    public void deposit(){
+        amountOf = 1;
+        System.out.println(amountOf);
+    };
+}
